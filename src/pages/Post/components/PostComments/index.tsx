650b969de6a92745a80ef51e7f1dc98db98c096b
Guilherme Braun <guiabraun@hotmail.com>
2022-10-22 20:00:34 -0300

    Update

--- src/pages/Post/components/PostComments/index.tsx
+++ src/pages/Post/components/PostComments/index.tsx
@@ -1,79 +1,78 @@
-import { useContext, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { api } from '../../../../lib/axios'
-import { Comments, PostCommentsContainer } from './styles'
+import { CommentForm, Comments, PostCommentsContainer } from './styles'
 import * as zod from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 
 interface PostCommentsProps {
-    commentsId?: string
+  commentsId?: string
 }
 
 interface CommentsType {
-    id: number
-    url: string
-    body: string
-    user: {
-        login: string
-        avatar_url: string
-    }
+  id: number
+  url: string
+  body: string
+  user: {
+    login: string
+    avatar_url: string
+  }
 }
 
 const commentValidationSchema = zod.object({
-    comment: zod.string(),
+  comment: zod.string(),
 })
 
 type DataProps = zod.infer<typeof commentValidationSchema>
 
 export function PostComments({ commentsId }: PostCommentsProps) {
-    const [comments, setComments] = useState<CommentsType[]>([])
-    const { register, handleSubmit} = useForm({
-        resolver: zodResolver(commentValidationSchema),
-        defaultValues:  {
-            comment: ''
-        }
-    })
+  const [comments, setComments] = useState<CommentsType[]>([])
+  const { register, handleSubmit } = useForm({
+    resolver: zodResolver(commentValidationSchema),
+    defaultValues: {
+      comment: '',
+    },
+  })
 
-
-    async function getComments() {
+  useEffect(() => {
+    if (commentsId) {
+      async function getComments() {
         const response = await api.get(
-            `/repos/guiibraun/github-blog/issues/${commentsId}/comments`,
+          `/repos/guiibraun/github-blog/issues/${commentsId}/comments`,
         )
         setComments(response.data)
+      }
+      getComments()
     }
-    useEffect(() => {
-        if (commentsId) {
-            getComments()
-        }
-    }, [commentsId])
+  }, [commentsId])
 
-
-    async function handleCommentSubmit(data: DataProps) {
-        const response = await api.post(
-            `/repos/guiibraun/github-blog/issues/${commentsId}/comments`,
-            {
-                owner: 'guiibraun',
-                repo: 'github-blog',
-                issue_number: Number(commentsId),
-                body: data.comment,
-            },
-        )
-        setComments(prevState => [response.data, ...prevState, ])
-    }
-
-    return (
-        <PostCommentsContainer>
-            <h4>Comentários</h4>
-            <form onSubmit={handleSubmit(handleCommentSubmit)}>
-                <textarea placeholder="Deixe um comentário" {...register('comment')} />
-                <button>Enviar</button>
-            </form>
-            {comments.map((comment) => (
-                <Comments key={comment.id}>
-                    <img src={comment.user.avatar_url} alt="" />
-                    <div>{comment.body}</div>
-                </Comments>
-            ))}
-        </PostCommentsContainer>
+  async function handleCommentSubmit(data: DataProps) {
+    const response = await api.post(
+      `/repos/guiibraun/github-blog/issues/${commentsId}/comments`,
+      {
+        owner: 'guiibraun',
+        repo: 'github-blog',
+        issue_number: Number(commentsId),
+        body: data.comment,
+      },
     )
+    console.log(response)
+    setComments((prevState) => [response.data, ...prevState])
+  }
+
+  return (
+    <PostCommentsContainer>
+      <h4>Comentários</h4>
+      <CommentForm onSubmit={handleSubmit(handleCommentSubmit)}>
+        <textarea placeholder="Deixe um comentário" {...register('comment')} />
+        <button type="submit">Enviar</button>
+      </CommentForm>
+      {comments.map((comment) => (
+        <Comments key={comment.id}>
+          <img src={comment.user.avatar_url} alt="" />
+          <div>{comment.body}</div>
+        </Comments>
+      ))}
+    </PostCommentsContainer>
+  )
 }
--- src/pages/Post/components/PostComments/styles.ts
+++ src/pages/Post/components/PostComments/styles.ts
@@ -10,3 +10,37 @@ export const Comments = styled.div`
     max-width: 100%;
   }
 `
+export const CommentForm = styled.form`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+
+  gap: 1rem;
+
+  textarea {
+    flex: 1;
+    padding: 1rem;
+    background-color: ${(prosp) => prosp.theme['base-input']};
+
+    color: ${(props) => props.theme['base-title']};
+
+    border: 1px solid ${(props) => props.theme['base-border']};
+    border-radius: 6px;
+
+    min-height: 6rem;
+
+    &::placeholder {
+      color: ${(props) => props.theme['base-label']};
+    }
+  }
+
+  button {
+    background-color: ${(props) => props.theme['base-post']};
+    color: ${(props) => props.theme['base-title']};
+    border: 0;
+    width: auto;
+    height: 58px;
+    border-radius: 6px;
+    cursor: pointer;
+  }
+`
